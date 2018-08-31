5642a36baf6913d8a175d33f086144777b585a76
OTR500miles2go <32044213+OTR500miles2go@users.noreply.github.com>
2018-08-31 15:22:50 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi world and FreeCodeCamp</h1>
+    <p>My name is Diane Gayden.</p>
+    <p>Thanks for helping me build great software.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
